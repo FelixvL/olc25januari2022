a786f1dff697c458b7c658444bd2091b5ccceb72
felixvl <digitaleservice@gmail.com>
2022-01-25 11:50:14 +0100

    daar gaan we

--- voorbeeld.py
+++ voorbeeld.py
@@ -1,1 +1,38 @@
+
 print("hoi")
+
+variabele1 = 3
+variabele1 = input()   # casten omzetten
+print(int(variabele1) + int(variabele1))
+
+def berekenen(mijnparam):
+	print("dit wil ik printen")
+	var1 = mijnparam
+	print("ik ga dit vermenigvuldigen")
+	print(mijnparam)
+	print("=")
+	print(mijnparam*mijnparam)
+	return "dinsdag"
+
+dedag = berekenen(7)
+berekenen(15)
+berekenen(77)
+print(dedag)
+
+
+
+
+
+
+
+
+
+
+#int variabele2 = 3;
+#//variabele2 = "dinsdag";
+#String variabele3 = "woensdag"
+
+#if(){    // java c c++ c# php javascript
+
+#} //
+
